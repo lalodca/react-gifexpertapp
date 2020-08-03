88161a82ebeccf2c3f2805531b7a35bf71e90984
Eduardo De La Cruz <edcruz17@gmail.com>
2020-08-02 19:24:51 -0500

    add docs

---
+++ docs/precache-manifest.d3193b1acee4cbe9709ed228d195be38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f3518b656528e4bbfc1e3cb86f7c18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f5f1d3cd89081675e3a",
+    "url": "/static/css/main.1041f713.chunk.css"
+  },
+  {
+    "revision": "2cdc7c9f4b6827f2475b",
+    "url": "/static/js/2.a5a9913f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a5a9913f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5f1d3cd89081675e3a",
+    "url": "/static/js/main.c4c1269d.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
